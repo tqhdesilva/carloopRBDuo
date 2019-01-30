5bf59360e51a9eb4d36dbaf6e9d4f588d28d6741
tqhdesilva <tdesilvaqihao@gmail.com>
2019-01-30 08:10:12 -0700

    did some stuff I don't remember

--- src/carloopRBDuo.ino
+++ src/carloopRBDuo.ino
@@ -8,9 +8,28 @@
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-
+  ble.init()
+  ble.onConnectedCallback(deviceConnectedCallback);
+  ble.onDisconnectedCallback(deviceDisconnectedCallback);
+  ble.onDataWriteCallback(gattWriteCallback);
 }
 
+void deviceConnectedCallback(BLEStatus_t status, uint16_t handle) {
+  switch (status) {
+    case BLE_STATUS_OK:
+      Serial.println("Device connected!");
+      break;
+    default: break;
+  }
+}
+
+void deviceDisconnectedCallback(uint16_t handle) {
+  Serial.println("Disconnected.");
+}
+
+int gattWriteCallback(uint16_t value_handle, uint8_t *buf, uint16_t size) {
+  Serial.println("GATT written.")
+}
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
